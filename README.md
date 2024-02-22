b4e671a89906394435325d2b72733bce316b2722
javiwebjr <urrutia_987@hotmail.com>
2024-02-22 13:57:43 -0600

    Nest - openAI

---
+++ README.md
@@ -0,0 +1,9 @@
+#Instalación en Dev
+
+1. Clonar el repositorio
+
+2. Instalar dependencias ```npm install```
+
+3. Crear archivo .env basado en el .env.template
+
+4. Ejecturas ```npm run start:dev```
---
+++ src/gpt/gpt.service.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+
+import { orthographyCheckUseCase } from './use-cases';
+import { OrthographyDto } from './dtos';
+
+@Injectable()
+export class GptService {
+
+
+    private openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    })
+    
+    
+    async orthographyCheck(orthographyDto: OrthographyDto) {
+        return await orthographyCheckUseCase( this.openai, {
+            prompt: orthographyDto.prompt
+        });
+    }
+}
